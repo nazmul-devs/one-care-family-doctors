f8113b6fadce77c85b63e0de0abbe977b160d77f
Nazmul Hosen <nazmulhosenm668@gmail.com>
2021-11-02 19:57:18 +0600

    important info added in home banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -41,6 +41,85 @@ const Banner = () => {
 					<img width="600" src={chair} alt="" />
 				</Grid>
 			</Grid>
+
+			<Grid
+				container
+				spacing={2}
+				sx={{ textAlign: "center", mt: -10, color: "#fff" }}
+			>
+				<Grid item xs={4}>
+					<Box>
+						<Grid
+							sx={{
+								px: 2,
+								justifyContent: "center",
+								display: "flex",
+								alignItems: "center",
+							}}
+							container
+							style={{ background: "#00DAD6" }}
+						>
+							<Grid item xs={4}>
+								<i className="far fa-clock"></i>
+							</Grid>
+							<Grid item xs={8}>
+								<Typography variant="h5">Opening Hours</Typography>
+								<Typography variant="p">
+									Lorem ipsum dolor sit amet. dolor sit amet.
+								</Typography>
+							</Grid>
+						</Grid>
+					</Box>
+				</Grid>
+				<Grid item xs={4}>
+					<Box>
+						<Grid
+							sx={{
+								px: 2,
+								justifyContent: "center",
+								display: "flex",
+								alignItems: "center",
+							}}
+							container
+							style={{ background: "#34495E", color: "#fff" }}
+						>
+							<Grid item xs={4}>
+								<i className="far fa-clock"></i>
+							</Grid>
+							<Grid item xs={8}>
+								<Typography variant="h5">Visit our location</Typography>
+								<Typography variant="p">
+									Banani R-15, D-block Dhaka
+								</Typography>
+							</Grid>
+						</Grid>
+					</Box>
+				</Grid>
+				<Grid item xs={4}>
+					<Box>
+						<Grid
+							sx={{
+								px: 2,
+								display: "flex",
+								alignItems: "center",
+							}}
+							container
+							style={{ background: "#00DAD6" }}
+						>
+							<Grid item xs={4}>
+								<i className="far fa-clock"></i>
+							</Grid>
+							<Grid item xs={8}>
+								<Typography variant="h5">Contact us now</Typography>
+								<Typography variant="p">
+									01705511718 Lorem ipsum dolor sit amet consectetur
+									adipisicing elit. Quam qui illum
+								</Typography>
+							</Grid>
+						</Grid>
+					</Box>
+				</Grid>
+			</Grid>
 		</Container>
 	);
 };
