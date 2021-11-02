a60232defbaaf4fefa017c19e56d74b577ec1ec0
Nazmul Hosen <nazmulhosenm668@gmail.com>
2021-11-02 17:43:26 +0600

    create react app and install react-router-dom and material ui

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,17 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
+const Home = () => {
+	return (
+		<div>
+			<h2>This is home</h2>
+			<Box>
+				<h2>hello</h2>
+				<Typography>helo</Typography>
+			</Box>
+		</div>
+	);
+};
+
+export default Home;
